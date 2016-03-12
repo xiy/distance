d3665bea8f1b854752a4da9ef8c98d16a9e52c43
Mark Anthony Gibbins <xiy3x0@gmail.com>
2016-03-12 14:30:40 +0000

    Initial commit

---
+++ lua/wrk.lua
@@ -0,0 +1,4 @@
+wrk.method = "POST"
+wrk.headers["Accept"] = "application/json"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = '{"source": "Hi Andrew\nI would be interested in getting to know you.\nsadly, you are not online. If you want, write me... ttutti911@gmail.com\nI will send photos by email.", "target": "Hi Mark\nI would be interested in getting to know you.\nsadly, you are not online. If you want, write me... ttutti911@gmail.com\nI will send photos by email." }'
