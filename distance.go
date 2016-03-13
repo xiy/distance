7c81ffd1ef7fdbd6b01e781eb057f07199838ec2
Mark Anthony Gibbins <xiy3x0@gmail.com>
2016-03-13 17:28:59 +0000

    Improve package layout and refactor a bit

--- distance.go
+++ distance.go
@@ -1,76 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	levenshtein "github.com/texttheater/golang-levenshtein/levenshtein"
-	"log"
-	"net/http"
-
-	"golang.org/x/net/context"
-
-	"github.com/go-kit/kit/endpoint"
-	httptransport "github.com/go-kit/kit/transport/http"
-)
-
-// DistanceService provides an interface to determining string similarity using
-// the Levenshtein algorithm.
-type DistanceService interface {
-	GetDistance(string, string) int
-}
-
-type distanceRequest struct {
-	Source string `json:"source"`
-	Target string `json:"target"`
-}
-
-type distanceResponse struct {
-	Distance int    `json:"distance"`
-	Err      string `json:"err,omitempty"`
-}
-
-type distanceService struct{}
+import "github.com/xiy/distance/levenshtein"
 
 func main() {
-	ctx := context.Background()
-	service := distanceService{}
-
-	distanceHandler := httptransport.NewServer(
-		ctx,
-		makeDistanceEndpoint(service),
-		decodeDistanceRequest,
-		encodeDistanceResponse)
-
-	http.Handle("/", distanceHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func makeDistanceEndpoint(service DistanceService) endpoint.Endpoint {
-	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		req := request.(distanceRequest)
-		distance := service.GetDistance(req.Source, req.Target)
-		return distanceResponse{distance, ""}, nil
-	}
-}
-
-func decodeDistanceRequest(r *http.Request) (interface{}, error) {
-	var request distanceRequest
-
-	log.Printf("[%s] request received.", r.Method)
-
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-
-	return request, nil
-}
-
-func encodeDistanceResponse(w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
-}
-
-func (distanceService) GetDistance(source string, target string) int {
-	return levenshtein.DistanceForStrings(
-		[]rune(source),
-		[]rune(target),
-		levenshtein.DefaultOptions)
+	levenshtein.Start()
 }
